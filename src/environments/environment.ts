a5f1ba54076b92b36255fe371c74304504fdb802
ELASRI Mohamed Youssef <elasri.mohamedyoussef@gmail.com>
2023-03-10 11:25:14 +0100

    starting

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment= {
+  firebase: {
+    projectId: 'meteo-6ae41',
+    appId: '1:189137861149:web:a434fb46a66d5bd6d5e4f4',
+    storageBucket: 'meteo-6ae41.appspot.com',
+    apiKey: 'AIzaSyAQqd4HZl5PqBxH0_sWyTi_FnfeghfU5fk',
+    authDomain: 'meteo-6ae41.firebaseapp.com',
+    messagingSenderId: '189137861149',
+    measurementId: 'G-6VN26MDQ2R',
+  },
+    production: false
+};
